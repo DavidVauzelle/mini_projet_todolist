c7e9ad9a629e79f5f82e18072bbfbf198993f64d
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-07-17 09:17:40 +0200

    Ajout de la bdd dans un dossier documentation.

---
+++ Documentation/mini_projet_todolist.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 17, 2024 at 07:16 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mini_projet_todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taches`
+--
+
+CREATE TABLE `taches` (
+  `id` int NOT NULL,
+  `nom_tache` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `taches`
+--
+
+INSERT INTO `taches` (`id`, `nom_tache`) VALUES
+(1, 'Faire le ménage'),
+(2, 'Faire une lessive'),
+(3, 'Faire les courses'),
+(7, 'Jouer à la PS5'),
+(25, 'Aller à la salle');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `taches`
+--
+ALTER TABLE `taches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `taches`
+--
+ALTER TABLE `taches`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
